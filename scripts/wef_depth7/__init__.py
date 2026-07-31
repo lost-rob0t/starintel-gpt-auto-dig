2041e6208dd0ebe5900fcb74bdd8a505cb19a094
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-31 04:49:26 -0400

    Add WEF depth 7 scraper package

---
+++ scripts/wef_depth7/__init__.py
@@ -0,0 +1,45 @@
+from .collectors import AuditorCollector, GitHubCollector, LegistarCollector, SiteCollector, WaybackCollector
+from .core import (
+    COLLECTORS,
+    CollectJob,
+    HttpClient,
+    Observation,
+    PageParser,
+    Stop,
+    TargetPlan,
+    keyword_hits,
+)
+from .runtime import (
+    CollectorActor,
+    WriterActor,
+    enumerate_jobs,
+    execute,
+    load_config,
+    main,
+    parse_args,
+    select_targets,
+)
+
+__all__ = [
+    "AuditorCollector",
+    "COLLECTORS",
+    "CollectJob",
+    "CollectorActor",
+    "GitHubCollector",
+    "HttpClient",
+    "LegistarCollector",
+    "Observation",
+    "PageParser",
+    "SiteCollector",
+    "Stop",
+    "TargetPlan",
+    "WaybackCollector",
+    "WriterActor",
+    "enumerate_jobs",
+    "execute",
+    "keyword_hits",
+    "load_config",
+    "main",
+    "parse_args",
+    "select_targets",
+]
